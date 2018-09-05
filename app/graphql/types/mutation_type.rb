e9e2e3e2f7322ed5385f0625d18e584832b26676
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2018-09-04 18:01:52 -0700

    Add additional graphql queries and mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,4 +1,27 @@
-module Types
-  class MutationType < Types::BaseObject
+Types::MutationType = GraphQL::ObjectType.define do
+  name "CreateInvestor"
+
+  field :createInvestor, Types::InvestorType do
+    argument :name, !types.String
+    argument :meeting_status, !types.String
+
+    resolve -> (obj, args, ctx) {
+      Investor.create(
+        name: args[:name],
+        meeting_status: args[:meeting_status]
+      )
+    }
+  end
+
+  field :editInvestor, Types::InvestorType do
+    argument :id, !types.ID
+    argument :meeting_status, !types.String
+
+    resolve -> (obj, args, ctx) {
+      investor = Investor.find(args[:id])
+      investor.meeting_status = args[:meeting_status]
+      investor.save
+      investor
+    }
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,4 +6,11 @@ Types::QueryType = GraphQL::ObjectType.define do
     # resolve would be called in order to fetch data for that field
     resolve -> (obj, args, ctx) { Investor.all }
   end
+
+  field :investor do
+    type Types::InvestorType
+    description "Fetch a single investor"
+    argument :id, !types.ID
+    resolve -> (obj, args, ctx) { Investor.find(args[:id]) }
+  end
 end
